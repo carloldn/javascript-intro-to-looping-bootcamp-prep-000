abff352d6be2bf46e2e959c71c8e50c3675b9a60
carlo montanaro <carloldn@outlook.com>
2018-09-23 21:45:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function whileLoop(n) {
     return "done";
   }
 function doWhileLoop(array) {
-  function maybeTrue() {
+  function incrementVariable() {
     return Math.random() >= 0.5
   }
    do {
