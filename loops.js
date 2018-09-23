79f42adc6e9970b6399f241599880b9a21343ebd
carlo montanaro <carloldn@outlook.com>
2018-09-23 21:17:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,16 +1,12 @@
 for (var i =1; i < 25;
 i++) {
-  console.log("I am one strange loop"+i+"time")
-})
-
-
-
-
-
-
-
-let whileloop = 35;
-while (countdown>0) {
-  console.log(-- 
-  done);
+  console.log("I am one strange loop"+i+"time");
+  
 }
+
+
+
+
+
+
+
