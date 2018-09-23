c4097882bb5ddbd52c44633726d83ce935595acf
carlo montanaro <carloldn@outlook.com>
2018-09-23 21:29:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop (array) {
       array.push("I am 1 strange loop")
     }
     else (i===0); {
-      array.push("I am ${i} strange loops.");
+      array.push("I am ${i} strange loop$.");
   }
 }
 return array;}
